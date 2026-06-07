e05c70c5cef3bd5b2a072e5d78f3f2faf8b2ab4a
Sanithu Malhiru <easanithumalhiru@gmail.com>
2026-06-07 15:24:56 +0530

    Add Vite logo SVG asset to the project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,10 @@
-import { image } from "framer-motion/client";
-
 export const projects = [
     {
         id: 1,
         title: "UniVerse",
         description: "Full-stack academic management system",
         tags: ["MongoDB","Express", "React", "Node.js"],
-        image: "./public/images/universe.png",
+        image: "./portfolio/public/assets/images/universe.png",
         link: "https://github.com/SanithuM/universe",
     },
     {
@@ -14,7 +12,7 @@ export const projects = [
     title: "Laced",
     description: "A high-performance e-commerce platform built for speed.",
     tags: ["FastAPI", "React", "PostgreSQL"],
-    image: "./public/images/laced.jpg",
+    image: "./portfolio/public/assets/images/laced.jpg",
     link: "https://github.com/ashen-v2/shoestore-group",
   },
   {
@@ -22,7 +20,7 @@ export const projects = [
     title: "Nike Clone",
     description: "A high quality clone of the Nike website",
     tags: ["React", "Tailwind CSS"],
-    image: "./public/images/nike.jpg",
+    image: "./portfolio/public/assets/images/nike.jpg",
     link: "https://github.com/SanithuM/nike-clone"
   },
   {
@@ -30,7 +28,7 @@ export const projects = [
     title: "DC Reimagined",
     description: "A DC Comics fan site with character profiles and news updates.",
     tags: ["React", "Tailwind CSS"],
-    image: "./public/images/dc_reimagined.jpg",
+    image: "./portfolio/public/assets/images/dc_reimagined.jpg",
     link: "https://github.com/SanithuM/dc-reimagined"
   }
 ];
